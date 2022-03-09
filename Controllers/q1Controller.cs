2ca8c4896b04bbb5e93c87d9c57821db1813d961
Tsania <tsnzzhr102@gmail.com>
2022-03-10 00:18:57 +0700

    src

---
+++ Controllers/q1Controller.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using q1.Models;
+
+namespace q1.Controllers;
+public class q1Controller : Controller
+{
+    public IActionResult Index(int id)
+    {
+        return View(id);
+    }
+
+    public IActionResult Profile(int id)
+    {
+        return View(id);
+    }
+    
+    public IActionResult Products(int id)
+    {
+        return View(id);
+    }
+
+    public IActionResult Collaborations(int id)
+    {
+        return View(id);
+    }
+
+    public IActionResult Company(int id)
+    {
+        return View(id);
+    }
+
+    public IActionResult SignIn(int id)
+    {
+        return View(id);
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
